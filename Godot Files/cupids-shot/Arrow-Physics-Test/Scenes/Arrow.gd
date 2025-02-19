9b1b479a537896921181880cdcd588bdfc223b0a
Jr <juniorachi128@hotmail.com>
2025-02-18 20:35:12 -0800

    lol

--- Godot Files/cupids-shot/Arrow-Physics-Test/Scenes/Arrow.gd
+++ Godot Files/cupids-shot/Arrow-Physics-Test/Scenes/Arrow.gd
@@ -19,10 +19,17 @@ var acceleration : Vector2
 #Two states really - arrowAiming is going to be probably for aiming but right now we're just trying to get the physics right
 var arrowLaunched = false
 
+var gliding = false
+var pointing_to = Vector2.ZERO
+
 var stuck = false
 
 var directional_influence = .5
 var direction : String
+
+#OK THIS MIGHT BE COOL
+var fuel : float = 100
+var boost :Vector2
 
 
 #Can work on the physics for this later, it's pretty weak feeling right now
@@ -31,20 +38,27 @@ func _process(delta: float) -> void:
 		true_velocity +=  gravity_direction * gravity
 		position +=  true_velocity * delta
 		rotation = true_velocity.angle() 
-		if true_velocity.x > 0:
+		if true_velocity.x > 0 && fuel > 10:
 			#true_velocity.x.clamp(0,true_velocity.x)
 			if Input.is_action_pressed("ui_left"):
-				true_velocity += Vector2(.2, -.01)
+				#Scuffed, really bad,
+				#Almost works though but spent too long on it
+				rotation = lerp(rotation,deg_to_rad(100),.1)
+				fuel -= .5
+				var temp_vel = true_velocity
+				true_velocity = lerp(temp_vel, boost,.5)
+				true_velocity.y -= .2
+				true_velocity -= Vector2(10,0)
 			elif Input.is_action_pressed("ui_right"):
-				true_velocity += Vector2(-.1, .4)
-		elif true_velocity.x < 0:
+				true_velocity.x -= .7
+				boost = Vector2(200,0) + true_velocity
+				
+		elif true_velocity.x < 0 && fuel > 10:
 			#true_velocity.x.clamp(true_velocity.x,0)
 			if Input.is_action_pressed("ui_left"):
 				true_velocity += Vector2(.2, -.01)
 			elif Input.is_action_pressed("ui_right"):
 				true_velocity += Vector2(-.1, .4)
-		pass
-	pass
 
 #run this when the state is changed
 func _launch_arrow(initial_velocity: Vector2) -> void:
