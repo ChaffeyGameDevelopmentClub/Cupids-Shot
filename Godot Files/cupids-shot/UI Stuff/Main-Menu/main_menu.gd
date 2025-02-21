3fa0fbe1c8bcc88dba0ad51d063c0bcce059e021
The Bron Jame Offical <91028605+TheBronJameOffical@users.noreply.github.com>
2025-02-20 21:24:27 -0800

    dear god
    options screen framework made
    
    tried adding in stuff from the UI plugin, why the fuck is it so god damn complex
    
    bunch new folders/files, literally just for options screen
    
    yay

--- Godot Files/cupids-shot/UI Stuff/Main-Menu/main_menu.gd
+++ Godot Files/cupids-shot/UI Stuff/Main-Menu/main_menu.gd
@@ -1,9 +1,23 @@
 extends Control
 
+# Currently screen is a little big, depends on how ya'll want the game to look tbh.
+# Also don't tell anybody but literally everything is stolen lmao
+
+# Vars to manipulate UI through code
+@export var OptionsPackedScene : PackedScene
+@export var OptionsContainer : MarginContainer
+@export var MenuContainer : MarginContainer
+@export var BackButton : Button
+
+# Label shit, might not need in the future anyways if we draw our logo
 @export var TitleLabel: Label
 
+# Vars to designate 
+var options_scene
+var sub_menu
+
 func _ready() -> void:
-	pass
+	_setup_options()
 
 func _process(delta: float) -> void:
 	pass
@@ -13,7 +27,26 @@ func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Arrow-Physics-Test/Scenes/test-stage-arrow.tscn")
 
 func _on_options_pressed() -> void:
-	pass
+	_open_sub_menu(options_scene)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _setup_options():
+	options_scene = OptionsPackedScene.instantiate()
+	options_scene.hide()
+	OptionsContainer.call_deferred("add_child", options_scene)
+
+func _open_sub_menu(menu : Control):
+	sub_menu = menu
+	sub_menu.show()
+	BackButton.show()
+	MenuContainer.hide()
+
+func _close_sub_menu():
+	if sub_menu == null:
+		return
+	sub_menu.hide()
+	sub_menu = null
+	BackButton.hide()
+	MenuContainer.show()
