9ff263dd8ddd5f5f78d6505483e97ab563bea3ca
ILikePotatoes127 <HuDaniel79@gmail.com>
2025-02-24 16:11:15 -0800

    boom

--- Godot Files/cupids-shot/Arrow-Physics-Test/Tileset Collection/Scripts/Balloon_Pop_Fun.gd
+++ Godot Files/cupids-shot/Arrow-Physics-Test/Tileset Collection/Scripts/Balloon_Pop_Fun.gd
@@ -17,6 +17,7 @@ func BalloonPop() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	print("heygafyusgdf")
 	if area.is_in_group("arrow"):
-		var dir = global_position - area.global_position;
+		var arrow = area.get_parent()
+		var dir = global_position - arrow.global_position;
 		area.true_velocity = area.true_velocity.bounce(dir.normalized())
 		BalloonPop()
