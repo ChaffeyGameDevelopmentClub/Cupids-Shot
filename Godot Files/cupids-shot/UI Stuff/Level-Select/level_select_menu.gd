c652fb27047beb93124304e715fc496940a7ed75
The Bron Jame Offical <91028605+TheBronJameOffical@users.noreply.github.com>
2025-02-25 16:54:22 -0800

    Pause menu n shit, oh god theres alort

--- Godot Files/cupids-shot/UI Stuff/Level-Select/level_select_menu.gd
+++ Godot Files/cupids-shot/UI Stuff/Level-Select/level_select_menu.gd
@@ -1,4 +1,25 @@
 extends Control
 
-func _on_test_lvl_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Arrow-Physics-Test/Scenes/test-stage-arrow.tscn")
+# Scene paths
+@export_file("*.tscn") var lvl1path : String
+@export_file("*.tscn") var lvl2path : String
+@export_file("*.tscn") var lvl3path : String
+@export_file("*.tscn") var lvl4path : String
+@export_file("*.tscn") var lvl5path : String
+
+# Buttons
+
+func _on_lv_l_1_button_pressed() -> void:
+	get_tree().change_scene_to_file(lvl1path)
+
+func _on_lv_l_2_button_pressed() -> void:
+	get_tree().change_scene_to_file(lvl2path)
+
+func _on_lv_l_3_button_pressed() -> void:
+	get_tree().change_scene_to_file(lvl3path)
+
+func _on_lv_l_4_button_pressed() -> void:
+	get_tree().change_scene_to_file(lvl4path)
+
+func _on_lv_l_5_button_pressed() -> void:
+	get_tree().change_scene_to_file(lvl5path)
