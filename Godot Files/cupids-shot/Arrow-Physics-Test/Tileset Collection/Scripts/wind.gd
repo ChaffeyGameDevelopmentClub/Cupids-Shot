eb949946473ee43b2588e4c05041793d3c30abcf
Jr <juniorachi128@hotmail.com>
2025-02-26 16:46:13 -0800

    wind fix

--- Godot Files/cupids-shot/Arrow-Physics-Test/Tileset Collection/Scripts/wind.gd
+++ Godot Files/cupids-shot/Arrow-Physics-Test/Tileset Collection/Scripts/wind.gd
@@ -1,5 +1,8 @@
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+func _ready() -> void:
+	animated_sprite_2d.play("default")
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("arrow"):
